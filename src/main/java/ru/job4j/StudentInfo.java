4354eee34e5adf2fc44bc05a04d1cfcf8279e8a9
borodadmiral <borodadmiral@gmail.com>
2026-03-16 13:33:32 +0300

    4.1. Разбор первой программы. [#159033 #34630]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Aleksei Ivanov");
+        System.out.println("1994");
+
+    }
+}
